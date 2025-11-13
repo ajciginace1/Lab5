41f105c88f4081f1c626297e97bb600fd8782fad
ajciginace1 <ajciginace1@my.smccd.edu>
2025-11-13 10:55:44 -0800

    Delete App.java

--- App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Scrabble!");
-        Tile tileV = new Tile('V',4);
-        System.out.println("Tile: " + tileV);
-    }
-}
